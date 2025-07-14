57aa2c3659bcb608fd62400735fdee516d6d5275
BangJu0620 <bju0620@naver.com>
2025-07-14 20:47:12 +0900

    [ feat ] Character, Item, Monster 클래스 생성

---
+++ TeamProjectSecond/TeamProjectSecond/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectSecond
+{
+    // 다른 클래스에서 사용하시려면
+    // Console.WriteLine(Character.Instance.Name); > 출력: 이름없음
+    // 저런 식으로 접근하면 됩니다.
+    // 캐릭터를 수정하시려면 private Character(){} 안에 있는 요소들을 수정하시면 됩니다.
+    public class Character
+    {
+        private static Character instance;
+
+        public static Character Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Character();
+                }
+                return instance;
+            }
+        }
+
+        private Character()
+        {
+            Level = 1;
+            Name = "이름없음";
+            Job = "초보자";
+            AttackPoint = 1;
+            DefensePoint = 1;
+            HealthPoint = 50;
+            Gold = 1500;
+        }
+
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int AttackPoint { get; set; }
+        public int DefensePoint { get; set; }
+        public int HealthPoint { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ TeamProjectSecond/TeamProjectSecond/Item.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectSecond
+{
+    // 다른 클래스에서 사용하시려면
+    // Console.WriteLine(Item.Instance[0].itemName); > 출력: 천 갑옷
+    // 저런 식으로 접근하면 됩니다.
+    // 아이템을 수정, 추가하고 싶으시면 static Item(){} 안에 있는 요소들을 수정, 추가하시면 됩니다.
+    public class Item
+    {
+        private static List<ItemData> instance;
+
+        public static List<ItemData> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new List<ItemData>();
+                }
+                return instance;
+            }
+        }
+
+        static Item()
+        {
+            instance = new List<ItemData>();
+            instance.Add(new ItemData("천 갑옷", "Armor", 0, 3, "얇지만 움직이기 쉬운 천 갑옷입니다.", 700, false, false));
+            instance.Add(new ItemData("수련자 갑옷", "Armor", 0, 5, "수련에 도움을 주는 갑옷입니다.", 1000, false, false));
+            instance.Add(new ItemData("무쇠갑옷", "Armor", 0, 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 1800, false, false));
+            instance.Add(new ItemData("스파르타의 갑옷", "Armor", 0, 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500, false, false));
+            instance.Add(new ItemData("낡은 검", "Weapon", 2, 0, "쉽게 볼 수 있는 낡은 검입니다.", 600, false, false));
+            instance.Add(new ItemData("짧은 단검", "Weapon", 3, 0, "빠르고 가볍지만 위력이 낮은 단검입니다.", 900, false, false));
+            instance.Add(new ItemData("청동 도끼", "Weapon", 5, 0, "어디선가 사용됐던거 같은 도끼입니다.", 1500, false, false));
+            instance.Add(new ItemData("스파르타의 창", "Weapon", 7, 0, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2700, false, false));
+        }
+    }
+
+    public class ItemData
+    {
+        public string itemName;
+        public string itemType;
+        public int itemAttackPoint;
+        public int itemDefensePoint;
+        public string itemDescription;
+        public int itemPrice;
+        public bool isOwned;
+        public bool isEquipped;
+
+        public ItemData(string iName, string iType, int iAP, int iDP, string iDescription, int iPrice, bool iOwned, bool iEquipped)
+        {
+            itemName = iName;
+            itemType = iType;
+            itemAttackPoint = iAP;
+            itemDefensePoint = iDP;
+            itemDescription = iDescription;
+            itemPrice = iPrice;
+            isOwned = iOwned;
+            isEquipped = iEquipped;
+        }
+    }
+}
