0d2036d32a6ee2aaa92d142f635b1c9e4b48a79a
Min545 <psychologic7272@gmail.com>
2025-07-16 16:43:38 +0900

    [ feat ] Accessory 추가
    속도, maxHPMP에 아이템이 접근 가능하도록 변경

--- TeamProjectSecond/TeamProjectSecond/Item.cs
+++ TeamProjectSecond/TeamProjectSecond/Item.cs
@@ -10,7 +10,8 @@ namespace TeamProjectSecond
     {
         Weapon,     // 무기
         Armor,      // 방어구
-        Consumable  // 소모품 (예: 포션)
+        Consumable,  // 소모품 (예: 포션)
+        Accessory
     }
     // 다른 클래스에서 사용하시려면
     // Console.WriteLine(Item.Instance[0].itemName); > 출력: 천 갑옷
@@ -51,6 +52,10 @@ namespace TeamProjectSecond
             instance.Add(new ItemData("짧은 단검", ItemType.Weapon, 3, 0, "빠르고 가볍지만 위력이 낮은 단검입니다.", 900, false, false));
             instance.Add(new ItemData("청동 도끼", ItemType.Weapon, 5, 0, "어디선가 사용됐던거 같은 도끼입니다.", 1500, false, false));
             instance.Add(new ItemData("스파르타의 창", ItemType.Weapon, 7, 0, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2700, false, false));
+
+            //Accessory 종류
+            instance.Add(new ItemData("신속의 신발", ItemType.Accessory, 0, 0, "속도를 높여주는 신발입니다.", 800, false, false, speed: 3));
+            instance.Add(new ItemData("체력의 반지", ItemType.Accessory, 0, 2, "최대 체력을 높여주는 반지입니다.", 800, false, false, 0, 30));
         }
 
         // 아이템 획득 로직
@@ -108,6 +113,9 @@ namespace TeamProjectSecond
         public int ItemPrice { get; set; }
         public bool IsOwned { get; set; }
         public bool IsEquipped { get; set; }
+        public int ItemSpeed { get; set; } = 0;
+        public int ItemMaxHP { get; set; } = 0;
+        public int ItemMaxMP { get; set; } = 0;
         public int ItemHealHPAmount { get; set; }
         public int ItemHealMPAmount { get; set; }
         public int Quantity { get; set; } = 0; //소모품 갯수
@@ -115,17 +123,21 @@ namespace TeamProjectSecond
 
         public ItemData() { }
 
-        public ItemData(string name, ItemType type, int atk, int def, string description, int price, bool owned, bool equipped, bool isShopItem = true)
+        public ItemData(string name, ItemType type, int atk, int def, string description, int price, bool owned, bool equipped, int speed = 0, int maxHP = 0, int maxMP = 0, bool isShopItem = true)
         {
             ItemName = name;
             ItemType = type;
             ItemAttackPoint = atk;
             ItemDefensePoint = def;
+            ItemSpeed = speed;
+            ItemMaxHP = maxHP;
+            ItemMaxMP = maxMP;
             ItemDescription = description;
             ItemPrice = price;
             IsOwned = owned;
             IsEquipped = equipped;
             IsShopItem = isShopItem;
+
             ParseHealAmountFromDescription(description);
         }
 
